3f4867384fe5cb78bedfc09c075a52d0b352b6cc
NHJ-macos <jwang0702@foxmail.com>
2022-01-10 01:01:04 +0800

    fix: bash

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ read id
 echo -n "请输入您的密码:"
 read password
 
-echo -n "请输入需要评教的教师姓名（输入0停止）"
-read name
-while(( $name!=5 ))
+echo -n "请输入需要评教的教师姓名"
+
+while read name
 do
-	python3 pj.py id password
-	echo -n "评教成功！"
+	python3 pj.py id password name
+	echo -n "评教成功，请输入下一位老师姓名"
 done
 echo -n "谢谢使用！"
