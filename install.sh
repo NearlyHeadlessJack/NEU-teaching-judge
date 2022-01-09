a7a695e7306dbf647ad75a2036f3890a7c77cf24
NHJ-macos <jwang0702@foxmail.com>
2022-01-10 00:53:56 +0800

    fix: change download url to gitee

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O pj.py https://raw.githubusercontent.com/NearlyHeadlessJack/NEU-teaching-judge/main/pj.py
-wget -O start.sh https://raw.githubusercontent.com/NearlyHeadlessJack/NEU-teaching-judge/main/start.sh
+wget -O pj.py https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/pj.py
+wget -O start.sh https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/start.sh
 ./start.sh
