9758eb3ea9d2277e13a81d2a724613a49121738b
NearlyHeadlessJack <jwang0702@foxmail.com>
2022-01-10 00:32:33 +0800

    upload: init commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -O pj.py https://raw.githubusercontent.com/NearlyHeadlessJack/NEU-teaching-judge/main/pj.py
+wget -O start.sh https://raw.githubusercontent.com/NearlyHeadlessJack/NEU-teaching-judge/main/start.sh
+./start.sh
