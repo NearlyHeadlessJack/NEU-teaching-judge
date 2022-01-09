3e68c227f5999558a8c955296d1a2dfadf794392
NHJ-macos <jwang0702@foxmail.com>
2022-01-10 00:55:40 +0800

    fix: bash

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O pj.py https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/pj.py
 wget -O start.sh https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/start.sh
-./start.sh
+source start.sh
