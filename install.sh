038452084bfc0a5ec603d1ad4b4413a7395527f4
NHJ-macos <jwang0702@foxmail.com>
2022-01-10 01:06:36 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 wget -O pj.py https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/pj.py
 wget -O start.sh https://gitee.com/NHJ2001/NEU-teaching-judge/raw/main/start.sh
-bash start.sh
+echo "运行 source start.sh 开始评教"
